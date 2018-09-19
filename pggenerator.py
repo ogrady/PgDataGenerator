c72044c11edf0b90cff47a3db7ec4e6f83badf07
Daniel O'Grady <danielogrady@gmx.de>
2018-09-19 14:38:25 +0200

    Include ForeignKey reference in generated schema

--- pggenerator.py
+++ pggenerator.py
@@ -3,9 +3,11 @@ import time
 import datetime as dt
 
 class Field(object):
-    def __init__(self, nullChance = 0):
+    def __init__(self, name, nullChance = 0):
+        self.name = name
         self.generated = []
         self.nullChance = nullChance
+        self.relation = None
 
     def schema(self):
         raise "Not implemented"
@@ -19,8 +21,8 @@ class Field(object):
         return val
 
 class IntField(Field):
-    def __init__(self, minVal = -2147483648, maxVal = 2147483647, nullChance = 0):
-        super(IntField, self).__init__(nullChance)
+    def __init__(self, name, minVal = -2147483648, maxVal = 2147483647, nullChance = 0):
+        super(IntField, self).__init__(name, nullChance)
         self.min = minVal
         self.max = maxVal
 
@@ -31,8 +33,8 @@ class IntField(Field):
         return rand.randint(self.min, self.max)
 
 class RealField(Field):
-    def __init__(self, precision = 6, minVal = -2147483648.0, maxVal = 2147483647.0, nullChance = 0):
-        super(RealField, self).__init__(nullChance)
+    def __init__(self, name, precision = 6, minVal = -2147483648.0, maxVal = 2147483647.0, nullChance = 0):
+        super(RealField, self).__init__(name, nullChance)
         self.format = "{:.%sf}" % (precision,)
         self.min = minVal
         self.max = maxVal
@@ -44,8 +46,8 @@ class RealField(Field):
         return self.format.format(rand.uniform(self.min, self.max))
 
 class BooleanField(Field):
-    def __init__(self, chanceForTrue = 0.5, nullChance = 0):
-        super(BooleanField, self).__init__(nullChance)        
+    def __init__(self, name, chanceForTrue = 0.5, nullChance = 0):
+        super(BooleanField, self).__init__(name, nullChance)        
         self.chanceForTrue = chanceForTrue
 
     def schema(self):
@@ -55,8 +57,8 @@ class BooleanField(Field):
         return rand.random() <= self.chanceForTrue
 
 class TextField(Field):
-    def __init__(self, minLength = 0, maxLength = 255, letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", nullChance = 0):
-        super(TextField, self).__init__(nullChance)
+    def __init__(self, name, minLength = 0, maxLength = 255, letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", nullChance = 0):
+        super(TextField, self).__init__(name, nullChance)
         self.minLength = minLength
         self.maxLength = maxLength
         self.letters = letters
@@ -65,14 +67,14 @@ class TextField(Field):
         return "TEXT"
 
     def generateNonNull(self):
-        return "'" + ''.join([rand.choice(self.letters) for _ in range(rand.randint(self.minLength, self.maxLength))]) + "'"
+        return "'%s'" % (''.join([rand.choice(self.letters) for _ in range(rand.randint(self.minLength, self.maxLength))]),)
 
 class DateField(Field):
-    def __init__(self, earliest, latest, nullChance = 0):
+    def __init__(self, name, earliest, latest, nullChance = 0):
         '''
         Both dates are expected to be a triple (year, month, day)
         '''
-        super(DateField, self).__init__(nullChance)
+        super(DateField, self).__init__(name, nullChance)
         self.earliest = time.mktime(earliest + (0,0,0,0,0,0)) # append hour, minute, second, ...
         self.latest = time.mktime(latest + (0,0,0,0,0,0))
 
@@ -80,11 +82,11 @@ class DateField(Field):
         return "DATE"
 
     def generateNonNull(self):
-        return "'" + dt.datetime.fromtimestamp(rand.randint(self.earliest, self.latest)).strftime('%Y-%m-%d') + "'"
+        return "'%s'" % (dt.datetime.fromtimestamp(rand.randint(self.earliest, self.latest)).strftime('%Y-%m-%d'),)
 
 class SerialField(Field):
-    def __init__(self, startValue = 1, nullChance = 0):
-        super(SerialField, self).__init__(nullChance)
+    def __init__(self, name, startValue = 1, nullChance = 0):
+        super(SerialField, self).__init__(name, nullChance)
         self.current = startValue
 
     def schema(self):
@@ -101,13 +103,14 @@ class ForeignKeyField(Field):
     at least some tuples for the referenced Field must be created already.
     Naturally, the values for the ForeignKeyField will only be whatever has been
     created up to that point in the referenced Field.
+    The type of the field will obviously be the same as the referenced field.
     '''
-    def __init__(self, reference, nullChance = 0):
-        super(ForeignKeyField, self).__init__(nullChance)
+    def __init__(self, name, reference, nullChance = 0):
+        super(ForeignKeyField, self).__init__(name, nullChance)
         self.reference = reference
 
     def schema(self):
-        return self.reference.schema()
+        return "%s REFERENCES %s.(%s)" % (self.reference.schema(), self.reference.relation.name, self.reference.name)
 
     def generateNonNull(self):
         assert len(self.reference.generated) > 0
@@ -117,9 +120,17 @@ class Relation(object):
     def __init__(self, name, fields):
         self.name = name
         self.fields = fields
+        for f in fields:
+            f.relation = self
+
+    def field(self, fname):
+        for f in self.fields:
+            if f.name == fname:
+                return f
+        return None
 
     def create(self):
-        return "CREATE TABLE %s(%s);" % (self.name, ', '.join(["%s %s" % (k,v.schema()) for k,v in self.fields.items()]))
+        return "CREATE TABLE %s(%s);" % (self.name, ', '.join(["%s %s" % (f.name,f.schema()) for f in self.fields]))
 
     def drop(self):
         return "DROP TABLE %s;" % (self.name)
@@ -127,47 +138,48 @@ class Relation(object):
     def insert(self, n):
         tuples = []
         for i in range(n):
-            tuples.append(', '.join([str(v.generate()) for v in self.fields.values()]))
+            tuples.append(', '.join([str(v.generate()) for v in self.fields]))
         return "INSERT INTO %s (VALUES \n %s\n);" % (self.name, ',\n '.join("(%s)" % (t,) for t in tuples))
 
-# print(DateField((1990,12,1), (2007,1,1)).generate())
-# print(TextField(20).generate())
-# print(BooleanField(0.5).generate())
-# print(IntField(10,100).generate())
-# print(RealField(6,-10,10).generate())
+def main():
+    print(DateField("some_data", (1990,12,1), (2007,1,1)).generate())
+    print(TextField("some_text", 20).generate())
+    print(BooleanField("some_bool", 0.5).generate())
+    print(IntField("some_int", 10,100).generate())
+    print(RealField("some_real", 6,-10,10).generate())
 
-# usage example:
-"""
-r1 = Relation("persons", {
-        "name": TextField(minLength = 5, maxLength = 10),
-        "income": IntField(10000, 40000),
-        "married": BooleanField(0.3),
-        "born": DateField((1970,1,1),(2010,4,10)),
-        "x": RealField(6,-1,1),
-        "mostlyNull": TextField(nullChance = 0.95),
-        "id": SerialField()
-    })
+    r1 = Relation("persons", [
+            TextField("name", minLength = 5, maxLength = 10),
+            IntField("income", 10000, 40000),
+            BooleanField("married", 0.3),
+            DateField("born", (1970,1,1),(2010,4,10)),
+            RealField("x", 6,-1,1),
+            TextField("mostlyNull", nullChance = 0.95),
+            SerialField("id")
+        ])
 
-r2 = Relation("orders", {
-        "pid": ForeignKeyField(r1.fields["id"]),
-        "quantity": IntField(1,10),
-        "article": TextField(maxLength = 20)
-    })
+    r2 = Relation("orders", [
+            ForeignKeyField("pid", r1.field("id")),
+            IntField("quantity", 1,10),
+            TextField("article", maxLength = 20)
+        ])
 
-r3 = Relation("workers", {
-        "worker": ForeignKeyField(r1.fields["name"]),
-        "boss": ForeignKeyField(r1.fields["name"])
-    })
+    r3 = Relation("workers", [
+            ForeignKeyField("worker", r1.field("name")),
+            ForeignKeyField("boss", r1.field("name"))
+        ])
 
-print(r1.drop())
-print(r1.create())
-print(r1.insert(3))
+    print(r1.drop())
+    print(r1.create())
+    print(r1.insert(3))
 
-print(r2.drop())
-print(r2.create())
-print(r2.insert(3))
+    print(r2.drop())
+    print(r2.create())
+    print(r2.insert(3))
 
-print(r3.drop())
-print(r3.create())
-print(r3.insert(3))
-"""
+    print(r3.drop())
+    print(r3.create())
+    print(r3.insert(30))
+
+if __name__ == '__main__':
+    main()
